8ff00c2168a7a19e315238964dadca468c920393
MaelleDaniel <maelledaniel2@gmail.com>
2021-09-01 15:41:33 +0200

    nav path

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,25 +11,25 @@ const Nav = () => {
     return (
         <StyledNav>
             <h1>
-                <Link id="logo" to="/">Capture</Link>
+                <Link id="logo" to="/React-studio/">Capture</Link>
             </h1>
             <ul>
                 <li>
-                    <Link to="/">1. About Us</Link>
+                    <Link to="/React-studio/">1. About Us</Link>
                     <Line
                         transition={{ duration: 0.75 }}
                         initial={{ width: "0%" }}
                         animate={{ width: pathname === "/" ? "50%" : "0%" }} />
                 </li>
                 <li>
-                    <Link to="/work">2. Our World</Link>
+                    <Link to="/React-studio/work">2. Our World</Link>
                     <Line
                         transition={{ duration: 0.75 }}
                         initial={{ width: "0%" }}
                         animate={{ width: pathname === "/work" ? "50%" : "0%" }} />
                 </li>
                 <li>
-                    <Link to="/contact">3.Contact Us</Link>
+                    <Link to="/React-studio/contact">3.Contact Us</Link>
                     <Line
                         transition={{ duration: 0.75 }}
                         initial={{ width: "0%" }}
